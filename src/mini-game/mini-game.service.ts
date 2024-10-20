35563738c55c262ca28321d054101d30b19b3e15
Rinx28 <minhanhpreeip@gmail.com>
2024-10-21 06:51:00 +0700

    debugs momemtjs

--- src/mini-game/mini-game.service.ts
+++ src/mini-game/mini-game.service.ts
@@ -4,7 +4,7 @@ import { Model } from 'mongoose';
 import { MiniGame } from './schema/mini.schema';
 import { UserService } from 'src/user/user.service';
 import { Cancel, Place, typeBet, typePlace } from './dto/dto.mini';
-import moment from 'moment';
+import * as moment from 'moment';
 import { SocketClientService } from 'src/socket/socket.service';
 import { SocketGateway } from 'src/socket/socket.gateway';
 import { UserBet } from 'src/user/schema/userBet.schema';
